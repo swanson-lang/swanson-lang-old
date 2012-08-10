221e63603ca13ea4975f66ed73877fc418014331
Douglas Creager <douglas.creager@redjack.com>
2012-08-10 09:19:24 -0400

    Assignables
    This patch creates a separate kind of value for memory regions, with
    explicit operations for retrieving and updating the contents of those
    regions.  We use Robert Harper's term “assignable” (instead of
    “variable” or “reference”) for this kind of entity.

--- include/swanson/kernel.h
+++ include/swanson/kernel.h
@@ -28,10 +28,11 @@ swan_kernel_get(struct swan_value *dest);
  * Opsets
  */
 
-void
-swan_opset_value(struct swan_value *dest, struct swan_opset *opset);
+bool
+swan_value_is_opset(struct swan_value *value);
 
-#define swan_value_is_opset(value)  ((value)->opset != NULL)
+int
+swan_opset_value(struct swan_value *dest, struct swan_opset *opset);
 
 CORK_ATTR_UNUSED
 static inline struct swan_opset *
@@ -39,17 +40,119 @@ swan_value_to_opset(struct swan_value *value)
 {
     if (CORK_UNLIKELY(value->content == NULL)) {
         swan_bad_value("Expected an opset, got an empty value");
+        return NULL;
+    } else if (CORK_UNLIKELY(!swan_value_is_opset(value))) {
+        swan_bad_value("Expected an opset");
+        return NULL;
+    } else {
+        return value->content;
     }
-    return value->content;
 }
 
 
 /*-----------------------------------------------------------------------
- * Memory management
+ * Types
  */
 
+struct swan_type {
+    size_t  instance_size;
+    struct swan_operation  *init;
+    struct swan_operation  *get;
+    struct swan_operation  *set;
+};
+
+bool
+swan_value_is_type(struct swan_value *value);
+
 int
-swan_explicit_allocator_new(struct swan_value *dest, struct swan_value *type);
+swan_type_new(struct swan_value *dest, size_t instance_size,
+              struct swan_operation *init, struct swan_operation *get,
+              struct swan_operation *set);
+
+CORK_ATTR_UNUSED
+static inline struct swan_type *
+swan_value_to_type(struct swan_value *value)
+{
+    if (CORK_UNLIKELY(value->content == NULL)) {
+        swan_bad_value("Expected an type, got an empty value");
+        return NULL;
+    } else if (CORK_UNLIKELY(!swan_value_is_type(value))) {
+        swan_bad_value("Expected an type");
+        return NULL;
+    } else {
+        return value->content;
+    }
+}
+
+
+/*-----------------------------------------------------------------------
+ * Assignables
+ */
+
+struct swan_assignable {
+    void  *content;
+    struct swan_operation  *get;
+    struct swan_operation  *set;
+    struct swan_operation  *unref;
+};
+
+bool
+swan_value_is_assignable(struct swan_value *value);
+
+CORK_ATTR_UNUSED
+static inline struct swan_assignable *
+swan_value_to_assignable(struct swan_value *value)
+{
+    if (CORK_UNLIKELY(value->content == NULL)) {
+        swan_bad_value("Expected an assignable, got an empty value");
+        return NULL;
+    } else if (CORK_UNLIKELY(!swan_value_is_assignable(value))) {
+        swan_bad_value("Expected an assignable");
+        return NULL;
+    } else {
+        return value->content;
+    }
+}
+
+int
+swan_assignable_alloc(struct swan_value *dest, struct swan_type *type);
+
+int
+swan_assignable_value(struct swan_value *dest, void *content,
+                      struct swan_operation *get, struct swan_operation *set,
+                      struct swan_operation *unref);
+
+
+/*-----------------------------------------------------------------------
+ * Arrays
+ */
+
+bool
+swan_value_is_fixed_array(struct swan_value *value);
+
+CORK_ATTR_UNUSED
+static inline void *
+swan_value_to_fixed_array(struct swan_value *value)
+{
+    if (CORK_UNLIKELY(value->content == NULL)) {
+        swan_bad_value("Expected a fixed-length array, got an empty value");
+        return NULL;
+    } else if (CORK_UNLIKELY(!swan_value_is_fixed_array(value))) {
+        swan_bad_value("Expected a fixed-length array");
+        return NULL;
+    } else {
+        return value->content;
+    }
+}
+
+int
+swan_fixed_array_alloc(struct swan_value *dest, struct swan_type *type,
+                       size_t element_count);
+
+int
+swan_fixed_array_value(struct swan_value *dest, void *elements,
+                       size_t element_size, size_t element_count,
+                       struct swan_operation *get, struct swan_operation *set);
 
 
 /*-----------------------------------------------------------------------
@@ -58,7 +161,11 @@ swan_explicit_allocator_new(struct swan_value *dest, struct swan_value *type);
 
 /* size */
 
-#define swan_value_is_size(value)  ((value)->opset != NULL)
+bool
+swan_value_is_size(struct swan_value *value);
+
+int
+swan_size_new(struct swan_value *dest, size_t value);
 
 CORK_ATTR_UNUSED
 static inline size_t *
@@ -66,8 +173,13 @@ swan_value_to_size(struct swan_value *value)
 {
     if (CORK_UNLIKELY(value->content == NULL)) {
         swan_bad_value("Expected a size, got an empty value");
+        return NULL;
+    } else if (CORK_UNLIKELY(!swan_value_is_size(value))) {
+        swan_bad_value("Expected a size");
+        return NULL;
+    } else {
+        return value->content;
     }
-    return value->content;
 }
 
 /* string */
--- tests/s0-evaluate/size-array-01.t
+++ tests/s0-evaluate/size-array-01.t
@@ -1,24 +1,24 @@
   $ swan s0 evaluate <<EOF
-  > size_type = kernel.size;
+  > size = kernel.size;
+  > size_type = size.type;
+  > size:~unref();
   > 
   > sizeof = kernel.sizeof;
   > _2 = sizeof.int16;
   > _4 = sizeof.int32;
   > sizeof:~unref();
   > 
-  > array_type = kernel.array(size_type, _4);
+  > array = kernel.array(size_type, _4);
   > size_type:~unref();
   > _4:~unref();
-  > alloc = kernel.explicit(array_type);
-  > array_type:~unref();
   > 
-  > array = alloc.alloc();
-  > element = array:claim(_2);
-  > element:print();
-  > array = element:return();
+  > element = array:"[]"(_2);
+  > value = element:"*"();
+  > value:print();
+  > value:~unref();
+  > element:~unref();
   > array:~unref();
   > _2:~unref();
   > 
-  > alloc:~unref();
   > EOF
   0
